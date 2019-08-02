fceb4d7d5a8fb447b996aff0ec25ec66cd5ddf64
CNXiaoHei <38616626+CNXiaoHei@users.noreply.github.com>
2019-08-02 15:51:35 +0800

    Update main_control.py

--- PID/main_control.py
+++ PID/main_control.py
@@ -29,11 +29,21 @@ class Tcp_server(Thread):
 class Main_Control:
     def __init__(self):
         self.pid = pid_control.PID_control()
-        self.db = MySQLdb.connect(host='192.168.1.103', user='root', passwd='201411', db='pyqt_schema')
+        self.db = MySQLdb.connect(host='192.168.1.103', user='root', passwd='201411', db='data_save')
+        self.db0 = self.db.cursor()
         self.server = socket(family=AF_INET, type=SOCK_STREAM)
         self.set_point = 0.0
 
     def dealwith_data(self, set_point):
+        """
+        数据格式应为
+        {
+            'name':'Pressure_Sensor',
+            'value':0.01,
+        }
+        :param set_point:
+        :return:
+        """
         self.set_point = set_point
         self.server.bind(('192.168.1.2', 5566))
         self.server.listen(512)
@@ -41,7 +51,21 @@ class Main_Control:
         data = Tcp_server(client).start()
         if data['name'] == 'Pressure_Sensor':
             value = data['value']
-            time = data['time']
-            self.pid.feedback = value
-            output, num = self.pid.loop(self.set_point, time)
-            
+            output, num = self.pid.loop(self.set_point, value)
+            # output输出给阀
+            self.into_sql(value, 1)
+        elif data['name'] == 'Angle_Sensor':
+            avalue = data['value']
+            self.into_sql(avalue, 2)
+
+    def into_sql(self, data, x):
+        if x == 1:
+            sql = 'INSERT INTO PressureSensor(id,p_value)VALUES(NULL,' + str(data) + ');'
+            self.db0.execute(sql)
+            self.db.commit()
+            self.db.close()
+        if x == 2:
+            sql = 'INSERT INTO AngleSensor(id,a_value)VALUES(NULL,' + str(data) + ');'
+            self.db0.execute(sql)
+            self.db.commit()
+            self.db.close()
