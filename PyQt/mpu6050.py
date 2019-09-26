5881eac3f94c484b3a26aed93d8e1347b90f2cd7
CNXiaoHei <38616626+CNXiaoHei@users.noreply.github.com>
2019-09-26 09:59:51 +0800

    Create mpu6050.py

---
+++ PyQt/mpu6050.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import smbus
+import math
+import time
+import os
+
+# Power management registers
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+
+
+class Mpu6050:
+    def __init__(self):
+        self.address = 0x68
+        self.bus = smbus.SMBus(1)
+        self.bus.write_byte_data(self.address, power_mgmt_1, 0)
+
+
+    def read_byte(self, adr):
+        return self.bus.read_byte_data(self.address, adr)
+
+    def read_word(self, adr):
+        high = self.bus.read_byte_data(self.address, adr)
+        low = self.bus.read_byte_data(self.address, adr + 1)
+        val = (high << 8) + low
+        return val
+
+    def read_word_2c(self, adr):
+        val = self.read_word(adr)
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
+
+    def dist(self, a, b):
+        return math.sqrt((a * a) + (b * b))
+
+    def get_y_rotation(self, x, y, z):
+        radians = math.atan2(x, self.dist(y, z))
+        return -math.degrees(radians)
+
+    def get_x_rotation(self, x, y, z):
+        radians = math.atan2(y, self.dist(x, z))
+        return math.degrees(radians)
+
